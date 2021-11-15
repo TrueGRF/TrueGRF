47cf1a4ac12a47c90cc7bbabcdf6ee0167f59ae3
Patric Stout <truebrain@openttd.org>
2021-11-15 20:18:44 +0000

    feat(sprites): change "blue" colour in transparent pixels

--- src/Sprite.tsx
+++ src/Sprite.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import './Sprite.css';
 
 const sprite_id_to_image: {[key: number]: any} = {
@@ -8,6 +10,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 37,
         height: 26,
+        rel_x: -17,
+        rel_y: -8,
     },
     2012: {
         filename: "coalmine_base.gimp.png",
@@ -15,6 +19,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 36,
         height: 50,
+        rel_x: -16,
+        rel_y: -33,
     },
     2013: {
         filename: "coalmine_anim1.gimp.png",
@@ -22,6 +28,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 58,
         height: 50,
+        rel_x: -16,
+        rel_y: -33,
     },
     2014: {
         filename: "coalmine_anim2.gimp.png",
@@ -29,6 +37,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 58,
         height: 50,
+        rel_x: -16,
+        rel_y: -33,
     },
     2015: {
         filename: "coalmine_anim3.gimp.png",
@@ -36,6 +46,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 58,
         height: 50,
+        rel_x: -16,
+        rel_y: -33,
     },
     2016: {
         filename: "coalmine_base.gimp.png",
@@ -43,6 +55,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 49,
         height: 32,
+        rel_x: -30,
+        rel_y: -9,
     },
     2017: {
         filename: "coalmine_base.gimp.png",
@@ -50,6 +64,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 48,
         height: 41,
+        rel_x: -29,
+        rel_y: -18,
     },
     2018: {
         filename: "coalmine_base.gimp.png",
@@ -57,6 +73,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 48,
         height: 41,
+        rel_x: -29,
+        rel_y: -18,
     },
     2019: {
         filename: "coalmine_base.gimp.png",
@@ -64,6 +82,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 36,
         height: 30,
+        rel_x: -16,
+        rel_y: -12,
     },
     2020: {
         filename: "coalmine_base.gimp.png",
@@ -71,6 +91,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 36,
         height: 43,
+        rel_x: -16,
+        rel_y: -25,
     },
     2021: {
         filename: "coalmine_base.gimp.png",
@@ -78,6 +100,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 36,
         height: 43,
+        rel_x: -16,
+        rel_y: -25,
     },
     2022: {
         filename: "coalmine_base.gimp.png",
@@ -85,6 +109,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 8,
         width: 64,
         height: 31,
+        rel_x: -31,
+        rel_y: 0,
     },
     2023: {
         filename: "coalmine_base.gimp.png",
@@ -92,6 +118,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 72,
         width: 64,
         height: 31,
+        rel_x: -31,
+        rel_y: 0,
     },
     2024: {
         filename: "coalmine_base.gimp.png",
@@ -99,6 +127,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 72,
         width: 64,
         height: 31,
+        rel_x: -31,
+        rel_y: 0,
     },
     2025: {
         filename: "coalmine_base.gimp.png",
@@ -106,6 +136,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 72,
         width: 64,
         height: 31,
+        rel_x: -31,
+        rel_y: 0,
     },
 
     // 2045-2054 power plant
@@ -115,6 +147,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 792,
         width: 43,
         height: 20,
+        rel_x: -21,
+        rel_y: 2,
     },
     2046: {
         filename: "industries_misc.png",
@@ -122,6 +156,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 792,
         width: 43,
         height: 56,
+        rel_x: -21,
+        rel_y: -34,
     },
     2047: {
         filename: "industries_misc.png",
@@ -129,6 +165,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 872,
         width: 43,
         height: 56,
+        rel_x: -21,
+        rel_y: -34,
     },
     2048: {
         filename: "industries_misc.png",
@@ -136,6 +174,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 872,
         width: 50,
         height: 29,
+        rel_x: -31,
+        rel_y: -4,
     },
     2049: {
         filename: "industries_misc.png",
@@ -143,6 +183,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 872,
         width: 50,
         height: 51,
+        rel_x: -31,
+        rel_y: -26,
     },
     2050: {
         filename: "industries_misc.png",
@@ -150,6 +192,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 872,
         width: 50,
         height: 58,
+        rel_x: -31,
+        rel_y: -33,
     },
     2051: {
         filename: "industries_misc.png",
@@ -157,6 +201,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 872,
         width: 30,
         height: 17,
+        rel_x: -14,
+        rel_y: -3,
     },
     2052: {
         filename: "industries_misc.png",
@@ -164,6 +210,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 872,
         width: 27,
         height: 28,
+        rel_x: -13,
+        rel_y: -14,
     },
     2053: {
         filename: "industries_misc.png",
@@ -171,6 +219,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 872,
         width: 34,
         height: 41,
+        rel_x: -13,
+        rel_y: -14,
     },
     2054: {
         filename: "industries_misc.png",
@@ -178,6 +228,8 @@ const sprite_id_to_image: {[key: number]: any} = {
         offset_y: 872,
         width: 49,
         height: 52,
+        rel_x: -26,
+        rel_y: -29,
     },
 }
 
@@ -229,13 +281,41 @@ const old_tile_to_sprite_id: {[key: number] : any} = {
 }
 
 function Sprite(props: any) {
+    const [dataUrl, setDataUrl] = useState("");
+
     if (props.old_tile_id === 0xfd) return (<span className="sprite" />);
 
     const gfx = old_tile_to_sprite_id[props.old_tile_id];
     const sprite = sprite_id_to_image[gfx.building] || sprite_id_to_image[gfx.ground];
 
+    const canvas = document.createElement("canvas");
+    canvas.width = sprite.width;
+    canvas.height = sprite.height;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) throw new Error("Could not get context");
+
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const sprite_image = new Image();
+    sprite_image.src = "sprites/" + sprite.filename;
+
+    sprite_image.onload = () => {
+        ctx.drawImage(sprite_image, sprite.offset_x, sprite.offset_y, sprite.width, sprite.height, 0, 0, sprite.width, sprite.height);
+
+        const img = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = img.data;
+        for (let i = 0; i < data.length; i += 4) {
+            if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 255) {
+                data[i + 3] = 0;
+            }
+        }
+        ctx.putImageData(img, 0, 0);
+        setDataUrl(canvas.toDataURL("image/png"));
+    };
+
     return (
-        <span className="sprite" style={{background: `url(sprites/${sprite.filename}) -${sprite.offset_x}px -${sprite.offset_y}px`, width: `${sprite.width}px`, height: `${sprite.height}px`}} />
+        <img src={dataUrl} className="sprite" alt="" style={{ left: `${sprite.rel_x + 32}px`, top: `${sprite.rel_y + 16}px` }} />
     );
 }
 
