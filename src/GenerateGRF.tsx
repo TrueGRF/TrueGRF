37d9d1710ab8a6823ee42868de3739dc5cf648b7
Patric Stout <truebrain@openttd.org>
2021-12-12 15:04:54 +0000

    feat: allow downloading the compiled GRF for local use

--- src/GenerateGRF.tsx
+++ src/GenerateGRF.tsx
@@ -1,6 +1,7 @@
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import { compile } from 'truegrf';
+import { saveAs } from 'file-saver';
 
 function GenerateGRF(props: any) {
     function Generate(newgame_seed: number) {
@@ -11,10 +12,16 @@ function GenerateGRF(props: any) {
         let game: any = document?.getElementById("game");
         game.contentWindow.openttd_inject_truegrf(newgrf_data, newgame_seed);
     }
+    function Download() {
+        let newgrf_data = compile({generic: props.generic, cargoes: props.cargoes, industries: props.industries});
+
+        saveAs(new Blob([newgrf_data]), "truegrf.grf");
+    }
 
     return (
         <Col className="text-end">
-            <Button variant="success" onClick={() => Generate(0)}>Reload GRF file in-game</Button>
+            <Button variant="warning" onClick={() => Download()}>Download</Button>
+            <Button variant="success" onClick={() => Generate(0)} style={{marginLeft: 10}}>Reload GRF file in-game</Button>
             <Button variant="success" onClick={() => Generate(1)} style={{marginLeft: 10}}>Reload GRF file in-game &amp; start new game</Button>
         </Col>
     );
