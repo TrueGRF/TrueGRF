ad29461a46af29b8abfe24a04ee3f68bd08d6db6
Patric Stout <truebrain@openttd.org>
2022-05-23 17:26:13 +0000

    feat(industry): make several industry-related fields editable
    Also make sure that renaming of industries works correctly, and
    enable industry syncing to GitHub.

---
+++ src/lib/components/industry/types.ts
@@ -0,0 +1,14 @@
+export const types = [
+    {
+        name: "Primary",
+        value: "primary",
+    },
+    {
+        name: "Secondary",
+        value: "secondary",
+    },
+    {
+        name: "Tertiary",
+        value: "tertiary",
+    },
+];
