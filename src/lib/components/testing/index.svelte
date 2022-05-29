09357cc11069bbf566e1fb9b0b8801fbc3d58c50
Patric Stout <truebrain@openttd.org>
2022-05-29 13:58:08 +0000

    feat(testing): notification during compiling
    FIRS for example takes ~2 seconds, so make it a bit more clear we
    actually responded to your compile request.

--- src/lib/components/testing/index.svelte
+++ src/lib/components/testing/index.svelte
@@ -9,30 +9,43 @@
     export let industries = undefined;
     export let general = undefined;
 
+    let compiling = false;
     let error = "";
 
     export const compileAndDownload = (config) => {
-        const result = compile(config);
-        if (result.getError()) {
-            error = result.getError();
-            return;
-        }
-        error = "";
+        compiling = true;
+        /* Delay, so Svelte can actually update the component. */
+        setTimeout(() => {
+            const result = compile(config);
+            compiling = false;
 
-        FileSaver.saveAs(new Blob([result.getOutput()]), "truegrf_" + new Date().toISOString() + ".grf");
+            if (result.getError()) {
+                error = result.getError();
+                return;
+            }
+            error = "";
+
+            FileSaver.saveAs(new Blob([result.getOutput()]), "truegrf_" + new Date().toISOString() + ".grf");
+        }, 10);
     };
 
     export const compileAndTest = (config, newgame_seed) => {
-        const result = compile(config);
-        if (result.getError()) {
-            error = result.getError();
-            return;
-        }
-        error = "";
+        compiling = true;
+        /* Delay, so Svelte can actually update the component. */
+        setTimeout(() => {
+            const result = compile(config);
+            compiling = false;
 
-        /* Tell OpenTTD to reload the GRF. */
-        const game = document?.getElementById("game");
-        game.contentWindow.openttd_inject_truegrf(result.getOutput(), newgame_seed);
+            if (result.getError()) {
+                error = result.getError();
+                return;
+            }
+            error = "";
+
+            /* Tell OpenTTD to reload the GRF. */
+            const game = document?.getElementById("game");
+            game.contentWindow.openttd_inject_truegrf(result.getOutput(), newgame_seed);
+        }, 10);
     };
 
     function TestNewgame() {
@@ -63,6 +76,15 @@
         <Button size="small" kind="primary" on:click={TestNewgame}>Test: new-game</Button>
         <Button size="small" kind="primary" on:click={TestReload}>Test: reload</Button>
     </div>
+    {#if compiling}
+        <InlineNotification
+            title="Compiling GRF ... "
+            subtitle="(this can take a minute)"
+            lowContrast
+            kind="info"
+            hideCloseButton
+        />
+    {/if}
     {#if error}
         <InlineNotification title="Error compiling GRF" subtitle={error} />
     {/if}
