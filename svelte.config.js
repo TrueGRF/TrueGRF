a0380a721a8d6543c1e58a8b055231b659e78446
Patric Stout <truebrain@openttd.org>
2022-06-01 09:22:17 +0000

    feat(changelog): show current version of TrueGRF and when clicked the changelog
    The changelog is dynamically loaded to reduce load on the GitHub API.
    Every version too is only rendered when opening, to further minimize
    the roundtrips needed to talk to GitHub.

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import { optimizeImports } from "carbon-preprocess-svelte";
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 
 const server = {};
+
+/* Abstract the version from the package.json file. */
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
 
 // Check if we are running in GitHub Codespaces.
 if (process.env['CODESPACE_NAME'] || process.env['GITPOD_INSTANCE_ID']) {
@@ -27,6 +34,9 @@ const config = {
 			base: process.env['BASE_PATH'] || '',
 		},
 		vite: {
+			define: {
+				__version__: JSON.stringify(pkg.version),
+			},
 			server: server,
 		}
 	}
